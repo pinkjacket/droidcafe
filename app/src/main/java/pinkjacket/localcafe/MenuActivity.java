6a12adb2d0089feccb7c8c646ade68bb416c948b
Student <example@example.com>
2018-03-15 15:32:39 -0700

    menu activity displays a menu

--- app/src/main/java/pinkjacket/localcafe/MenuActivity.java
+++ app/src/main/java/pinkjacket/localcafe/MenuActivity.java
@@ -1,13 +1,26 @@
 package pinkjacket.localcafe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MenuActivity extends AppCompatActivity {
+    @BindView(R.id.menuListView) ListView menuListView;
+
+    private String[] menuitems = new String[] {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, menuitems);
+        menuListView.setAdapter(adapter);
     }
 }
