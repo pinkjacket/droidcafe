812bc95e8bd72dc8674ee85d991743c3c7ef0c43
Student <example@example.com>
2018-03-15 11:46:41 -0700

    basic navigation buttons with one toast added

--- app/src/main/java/pinkjacket/localcafe/MainActivity.java
+++ app/src/main/java/pinkjacket/localcafe/MainActivity.java
@@ -2,12 +2,30 @@ package pinkjacket.localcafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.aboutButton) Button aboutButton;
+    @BindView(R.id.menuButton) Button menuButton;
+    @BindView(R.id.contactButton) Button contactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        menuButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
